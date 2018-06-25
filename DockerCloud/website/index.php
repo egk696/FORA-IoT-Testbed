3caf57dec8c8bfd298a86bddcb0dbe9c94db482d
Tórur Biskopstø Strøm <torur.strom@gmail.com>
2018-06-25 16:38:06 +0200

    docker: Working test example

--- DockerCloud/website/index.php
+++ DockerCloud/website/index.php
@@ -6,14 +6,14 @@
     <body>
         <h1>Welcome to my hello world page</h1>
         <ul>
-            <!-- <?php
-            $json = file_get_contents('http://product-service/');
+            <?php
+            $json = file_get_contents('http://data-integration:80/');
             $obj = json_decode($json);
             $products = $obj->products;
             foreach ($products as $product) {
                 echo "<li>$product</li>";
             }
-            ?> -->
+            ?>
         </ul>
     </body>
 </html>
