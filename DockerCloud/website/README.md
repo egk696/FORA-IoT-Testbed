cf324898fb776e223f4c149eaea28aefec06612d
Eleftherios Kyriakakis <elky@dtu.dk>
2018-06-26 22:35:55 +0200

    Dashboard website first commit

---
+++ DockerCloud/website/README.md
@@ -0,0 +1,12 @@
+# IoT-Cloud-Testbed Dashboard
+Visualizes probability of rain, latest sensor data and provides report logging. 
+
+## Features
+- Responsive mobile-friendly design
+
+
+# Test and Run
+1) Simply start the script 'webservice.py':
+         $ python webservice.py <port>
+
+2) Navigate to http://localhost:8080/
---
+++ DockerCloud/website/webservice.py
@@ -0,0 +1,20 @@
+import SimpleHTTPServer
+import SocketServer
+
+def run(port=8080):
+    Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+    httpd = SocketServer.TCPServer(("0.0.0.0", port), Handler)
+    print "serving at port", port
+    httpd.serve_forever()
+
+def get_bytes_from_file(filename):  
+    return open(filename, "rb").read() 
+
+if __name__ == "__main__":
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
+
